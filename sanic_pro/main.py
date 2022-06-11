13eaefa4a014efb576712bd13a2bf67a8d5aeefb
MichaelZhangs <58901344+MichaelZhangs@users.noreply.github.com>
2022-06-11 10:50:53 +0800

    Add files via upload

--- sanic_pro/main.py
+++ sanic_pro/main.py
@@ -1,15 +1,6 @@
-from settings import app
-from routers.routers_list import view_bp
-from utils.init_scheduler import InitScheduler
-from config.configs import redis_host, server_port
+from init import app
+from config.configs import server_host, server_port
 
 
-@view_bp.listener("before_server_start")
-async def setup_connection(app, loop):
-    print("开始连接------")
-    app.scheduler = InitScheduler().apscheduler_init()
-
-
-app.blueprint(view_bp)
 if __name__ == '__main__':
-    app.run(host=redis_host, port=server_port)
+    app.run(host=server_host, port=server_port, debug=True)
